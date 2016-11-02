97486bc9ea24d93ecfe89120d9bf3b6922846036
mycoolrobotyes <john@sl-interphase.com>
2016-11-01 19:29:48 -0700

    more code has been added

---
+++ Drive.c
@@ -0,0 +1,77 @@
+
+int deadband (int stick)
+{
+	const int db = 7;
+	int astick = abs(stick);
+	int ret = 0;
+	if(astick > db)
+	{
+		astick = pow(1.1,astick/1.5);
+
+		if(astick > 127) astick = 127;
+
+		if(stick < 0) astick *= -1;
+		ret = astick;
+	}
+	return ret;
+}
+
+void Drive(void)
+{
+	static int launcherButton_pv = 0;
+	static int launching = 0;
+
+		// motors driven by the sticks
+		motor[leftMotor] = deadband(vexRT(Ch3));
+		motor[rightMotor] = deadband(vexRT(Ch2));
+		motor[leftmotorback] = deadband(vexRT(Ch3));
+		motor[rightmotorback] = deadband(vexRT(Ch2));
+
+		if(vexRT[Btn7UXmtr2] == 1)
+		{
+			setLauncherSpeed (94);
+		}
+
+		if(vexRT[Btn7RXmtr2] == 1)
+		{
+			setLauncherSpeed (100);
+		}
+
+		if(vexRT[Btn7DXmtr2] == 1)
+		{
+			setLauncherSpeed (106);
+		}
+
+		if(vexRT[Btn5UXmtr2] == 1)
+ 		 {
+		   backwardslauncher();
+		  }
+
+
+		 if(vexRT[Btn5DXmtr2] == 1)
+ 		 {
+		   launcherOn();
+		  }
+		  else
+		  {
+		  	launcherOff();
+		  }
+
+    if(vexRT[Btn6UXmtr2] == 1)
+ 		 {
+		   motor[ballgrabbythingy] = 127;
+		   motor[ballgrabbythingy2] = 127;
+		  }
+		  else if(vexRT[Btn6DXmtr2] == 1)
+  		{
+ 		  motor[ballgrabbythingy] = -127;
+ 		 	motor[ballgrabbythingy2] = -127;
+  		}
+  		else
+ 		 {
+	  	 motor[ballgrabbythingy] = 0;
+	     motor[ballgrabbythingy2] = 0;
+    	}
+
+
+ }
