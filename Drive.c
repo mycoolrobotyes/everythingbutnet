5a79c36f0140c281d4e82e1188be486e987db48d
mycoolrobotyes <john@sl-interphase.com>
2016-11-01 20:11:37 -0700

    made a fix

--- Drive.c
+++ Drive.c
@@ -18,60 +18,12 @@ int deadband (int stick)
 
 void Drive(void)
 {
-	static int launcherButton_pv = 0;
-	static int launching = 0;
 
 		// motors driven by the sticks
 		motor[LRmotor] = deadband(vexRT(Ch3));
 		motor[RFmotor] = deadband(vexRT(Ch2));
 		motor[LFmotor] = deadband(vexRT(Ch3));
 		motor[RRmotor] = deadband(vexRT(Ch2));
-
-		if(vexRT[Btn7UXmtr2] == 1)
-		{
-			setLauncherSpeed (94);
-		}
-
-		if(vexRT[Btn7RXmtr2] == 1)
-		{
-			setLauncherSpeed (100);
-		}
-
-		if(vexRT[Btn7DXmtr2] == 1)
-		{
-			setLauncherSpeed (106);
-		}
-
-		if(vexRT[Btn5UXmtr2] == 1)
- 		 {
-		   backwardslauncher();
-		  }
-
-
-		 if(vexRT[Btn5DXmtr2] == 1)
- 		 {
-		   launcherOn();
-		  }
-		  else
-		  {
-		  	launcherOff();
-		  }
-
-    if(vexRT[Btn6UXmtr2] == 1)
- 		 {
-		   motor[ballgrabbythingy] = 127;
-		   motor[ballgrabbythingy2] = 127;
-		  }
-		  else if(vexRT[Btn6DXmtr2] == 1)
-  		{
- 		  motor[ballgrabbythingy] = -127;
- 		 	motor[ballgrabbythingy2] = -127;
-  		}
-  		else
- 		 {
-	  	 motor[ballgrabbythingy] = 0;
-	     motor[ballgrabbythingy2] = 0;
-    	}
 
 
  }
