def9b2e7837a65f2e430528ed4b6b8e57f1f1688
~Rebekah Julicher~ <rebekah@julicher.net>
2016-11-11 18:59:55 -0700

    Autonomous2 Finished (Not tested)

--- Autonomous2.c
+++ Autonomous2.c
@@ -1,4 +1,4 @@
-const char *autonomous2Name = "Auto2";
+const char *autonomous2Name = "Autonomous2";
 
 void Autonomous2(void)
 {
@@ -10,14 +10,20 @@ void Autonomous2(void)
 	wait10Msec(100);
 	drive(127,-127);
 	wait10Msec(500);
+	drive(127,127);
+	wait10Msec(100);
 	drive(0,0);
-	wait10Msec(10);
 	launcher(127);
 	wait10Msec(300);
 	launcher(0);
 	drive(-127,127);
 	wait10Msec(500);
 	drive(0,0);
+	wait10Msec(10);
+	drive(127,-127);
+	wait10Msec(500);
+	drive(0,0);
+	wait10Msec(10);
 }
 
 }
