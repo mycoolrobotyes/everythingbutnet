87942d6f3db93788c9423e214baa3c38f00d14dc
mycoolrobotyes <john@sl-interphase.com>
2017-02-07 19:02:00 -0700

    update

--- Autonomous1.c
+++ Autonomous1.c
@@ -6,14 +6,5 @@ void Autonomous1(void)
 	FastForwardForTime(78);
 	wait10Msec(PartTime);
   LeftTurnForTime(60);
-  FastForwardForTime(80);
-  ClawClose();
-  ArmupWithClaw();
-  RightTurnForTime(50);
-  ClawClose();
-	FastForwardForTime(130);
-	ClawClose();
-	LauncherDown(800);
-	ClawOpen2();
-	wait10Msec(PartTime);
+  FastForwardForTime(50);
 }
