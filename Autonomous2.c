d1f99604bca4b284bdb696b7167704a6b333a45d
Joseph Julicher <c10948@MacBook-Pro.local>
2017-02-18 12:50:43 -0700

    debugging autonomous
    added some LCD print statements to the autonomous modes for debugging

--- Autonomous2.c
+++ Autonomous2.c
@@ -2,6 +2,10 @@ const char *autonomous2Name = "simple";
 
 void Autonomous2(void)
 {
+  clearLCDLine(1);
+  setLCDPosition(1, 0);
+  displayNextLCDString("Running simple");
+
 	FastForwardForTime(156);
 	wait10Msec(PartTime);
 	ClawClose2();
