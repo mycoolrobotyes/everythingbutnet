29c45523f9dfe849ce25b08555f0df14eddcd328
Joseph Julicher <c10948@MacBook-Pro.local>
2017-02-18 10:00:43 -0700

    regenerated the documentation

--- documentation/html/search/all_73.js
+++ documentation/html/search/all_73.js
@@ -4,7 +4,7 @@ var searchData=
   ['smalltime',['SmallTime',['../_autonomous_functions_8c.html#ae66e6b2fea579036ed3461b48aeaf3e4',1,'AutonomousFunctions.c']]],
   ['speedgood',['speedgood',['../_motor_functions_8c.html#a41f26da7163b6da3722cc2837e7ab515',1,'MotorFunctions.c']]],
   ['speedsetpoint',['speedSetPoint',['../_motor_functions_8c.html#a8cd97bbec16a4107c2f40f90198be0cd',1,'MotorFunctions.c']]],
-  ['starwars',['STARWARS',['../_r_t_t_t_l_8c.html#aee5c3af75f169ce1a67b701f7970d51c',1,'STARWARS():&#160;RTTTL.c'],['../_r_t_t_t_l_8c.html#ac8417d4e80c1bae150b28ba0a9533518',1,'StarWars():&#160;RTTTL.c']]],
+  ['starwars',['StarWars',['../_r_t_t_t_l_8c.html#ac8417d4e80c1bae150b28ba0a9533518',1,'StarWars():&#160;RTTTL.c'],['../_r_t_t_t_l_8c.html#aee5c3af75f169ce1a67b701f7970d51c',1,'STARWARS():&#160;RTTTL.c']]],
   ['steponetime',['StepOneTime',['../_autonomous_skills_8c.html#aee602d0364693c4cef524284ed5a2881',1,'AutonomousSkills.c']]],
   ['stopmusic',['stopMusic',['../_r_t_t_t_l_8c.html#a7473e9a04dc2e8f05241857d86586194',1,'RTTTL.c']]]
 ];
